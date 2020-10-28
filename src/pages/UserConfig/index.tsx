ef607969259eac883f4e8da3ef25274d37815cf0
bkkater <biancamedeiros.bs10@gmail.com>
2020-10-28 16:06:21 -0300

    [add] UserConfig page

---
+++ src/pages/UserConfig/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { View, Image, Text, ScrollView } from 'react-native';
+
+import CompanyHeader from '../../components/CompanyHeader';
+import { Feather } from '@expo/vector-icons';
+
+import style from './styles';
+import ConfigOption from '../../components/ConfigOption';
+
+const UserConfig: React.FC = () => {
+  return (
+    <ScrollView>
+      <CompanyHeader user='@paulinhacptg' />
+      <View style={{ padding: 20 }}>
+        <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 50 }}>
+          <Feather name="user" size={40} color="black" />
+
+          <View style={{ width: '100%', marginLeft: 15 }}>
+            <Text style={style.profileNameText}>Paula Tavares</Text>
+          </View>
+        </View>
+
+        <View>
+          <ConfigOption name='Mensagens' description='Minhas conversas' icon='message-circle' />
+          <ConfigOption name='Minha conta' description='Configurações do perfil' icon='edit' />
+          <ConfigOption name='Carteira' description='Meu saldo e QR Code' icon='credit-card' />
+          <ConfigOption name='Configurações' description='' icon='settings' />
+          <ConfigOption name='Ajuda' description='' icon='help-circle' />
+          <ConfigOption name='Sair' description='' icon='log-out' />
+        </View>
+
+      </View>
+    </ScrollView>
+  );
+}
+
+export default UserConfig;
---
+++ src/pages/UserConfig/styles.ts
@@ -0,0 +1,24 @@
+import { StyleSheet, Image } from 'react-native';
+
+const styles = StyleSheet.create({
+    profileNameText: {
+        fontFamily: 'Ubuntu_500Medium',
+        fontSize: 22,
+        color: '#323232'
+    },
+    profileType: {
+        backgroundColor: '#323232',
+        borderRadius: 4,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 7,
+        marginTop: 11,
+        width: '40%'
+    },
+    profileTypeText: {
+        fontFamily: 'Ubuntu_500Medium',
+        color: '#FFF'
+    },
+})
+
+export default styles;
