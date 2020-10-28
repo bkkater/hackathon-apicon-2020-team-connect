100749f1f8976c60717aad4eeb11fac596ed44b3
bkkater <biancamedeiros.bs10@gmail.com>
2020-10-28 16:06:34 -0300

    [add] Profile page

---
+++ src/pages/Profile/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+
+import { ScrollView } from 'react-native-gesture-handler';
+
+import { StatusBar } from 'expo-status-bar';
+import ProfileImages from '../../components/ProfileImages';
+import ProfileInformation from '../../components/ProfileInformation';
+import CompanyHeader from '../../components/CompanyHeader';
+
+function Profile() {
+  return (
+    <View style={{ flex: 1 }}>
+      <StatusBar style='dark' />
+      <CompanyHeader user='@malubolos'/>
+
+      <ScrollView>
+        <View>
+          <ProfileInformation/>
+
+          <ProfileImages />
+
+        </View>
+
+      </ScrollView>
+    </View>
+
+  )
+}
+
+export default Profile;
