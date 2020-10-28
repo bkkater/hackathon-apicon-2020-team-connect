9c1eb0f7db9be6948b090ba3ddfaed9d4ba0a518
bkkater <biancamedeiros.bs10@gmail.com>
2020-10-28 19:26:22 -0300

    [add] profile info

---
+++ src/utils/profiles.ts
@@ -0,0 +1,50 @@
+export const profiles = [
+    {   
+        id: '01',
+        name: 'Pipocas gourmet da Lu',
+        type:'Doceria',
+        user: '@pipocaslulu',
+        distance: '210m',
+        image_uri:'https://images.unsplash.com/photo-1574201742421-fffd6af7a680?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
+    },
+    {   
+        id: '02',
+        name: 'Serralharia do joao',
+        type:'Serralheria',
+        user: '@serralheiro_joao',
+        distance: '326m',
+        image_uri:'https://images.unsplash.com/photo-1550773231-59c9530140fd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
+    },
+    {   
+        id: '03',
+        name: 'Verdes saudáveis',
+        type:'Mercado',
+        user: '@verdesaudaveis',
+        distance: '326m',
+        image_uri:'https://images.unsplash.com/photo-1493882946517-213a21fe70f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
+    },
+    {   
+        id: '04',
+        name: 'DuBem saladaria',
+        type:'Saladaria',
+        user: '@salada_dubem',
+        distance: '1.4km',
+        image_uri:'https://images.unsplash.com/photo-1572449043416-55f4685c9bb7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80'
+    },
+    {   
+        id: '05',
+        name: 'Hudson Music House',
+        type:'Loja de Música',
+        user: '@hudsonmusic',
+        distance: '2.7km',
+        image_uri:'https://images.unsplash.com/photo-1562092083-bf2848da4bcd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'
+    },
+    {   
+        id: '06',
+        name: 'Hudson Music House',
+        type:'Loja de Música',
+        user: '@hudsonmusic',
+        distance: '1.4km',
+        image_uri:'https://images.unsplash.com/photo-1572449043416-55f4685c9bb7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80'
+    }
+]
