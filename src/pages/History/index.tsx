f68dbbd6b37c0536e61721758ecfe47aba0317bb
bkkater <biancamedeiros.bs10@gmail.com>
2020-10-28 21:25:14 -0300

    [add] History page

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { ScrollView, View, Text } from 'react-native';
+import HistoryCard from '../../components/HistoryCard';
+import HomeHeader from '../../components/HomeHeader';
+import SearchField from '../../components/SearchField';
+import { productsHistory } from '../../utils/productsHistory';
+
+
+
+import style from './styles';
+
+const History: React.FC = () => {
+    return (
+        <ScrollView style={{ padding: 20, marginBottom: 70 }}>
+            <HomeHeader qr={false} />
+
+            <Text style={style.sectionTitle}>Histórico de Compras</Text>
+
+            <SearchField placeholder='Busque pelo produto ou nome do negócio' user={false} />
+
+            {
+                productsHistory.map((productsHistory) => (
+                    <HistoryCard orderCompany={productsHistory} />
+                ))
+            }
+
+        </ScrollView>
+
+    );
+}
+
+export default History;
---
+++ src/pages/History/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    sectionTitle: {
+        fontFamily: 'Ubuntu_500Medium',
+        fontSize: 20,
+        marginVertical: 10,
+        marginBottom: 20
+    }
+});
+
+
+export default styles;
