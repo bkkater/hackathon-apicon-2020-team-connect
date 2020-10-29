c7fd5fcf21fe114375c9066c58a084a6929e51ba
bkkater <biancamedeiros.bs10@gmail.com>
2020-10-28 21:27:00 -0300

    [add] utils file

---
+++ src/utils/productsHistory.ts
@@ -0,0 +1,29 @@
+export const productsHistory = [
+    {
+        image_uri: 'https://images.unsplash.com/photo-1557925923-33b27f891f88?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80',
+        name: 'Malu Bolos',
+        user: '@malubolos',
+        price: 35,
+        quantity: 1,
+        product: 'Bolo de chocolate',
+        date: '15/10/20',
+    },
+    {
+        image_uri: 'https://images.unsplash.com/photo-1510229955695-588e1612a69b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80',
+        name: 'Mundo das miçangas',
+        user: '@mundo_dasmicangas',
+        price: 25,
+        quantity: 1,
+        product: 'Conjunto sereia - Azul cobalto',
+        date: '10/10/20',
+    },
+    {
+        image_uri: 'https://images.unsplash.com/photo-1566109964132-ec243417ca2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+        name: 'Bar água de coco',
+        user: '@cocobar',
+        price: 9,
+        quantity: 1,
+        product: 'Água de coco - Gelada',
+        date: '28/09/20',
+    },
+]
