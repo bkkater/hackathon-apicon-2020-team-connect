f038be6d181338582f27b95fa3a891f285ba6fba
bkkater <biancamedeiros.bs10@gmail.com>
2020-10-28 21:26:49 -0300

    [add] HistoryCard

---
+++ src/components/HistoryCard/index.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+
+import style from './styles';
+
+interface HistoryCardProps {
+    orderCompany: {
+        image_uri: string,
+        name: string,
+        user: string,
+        price: number,
+        quantity: number,
+        product: string,
+        date: string,
+    }
+}
+
+const HistoryCard: React.FC<HistoryCardProps> = ({ orderCompany }) => {
+    return (
+        <View style={style.container}>
+            <View style={style.content}>
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                    <Image
+                        source={{ uri: orderCompany.image_uri }}
+                        style={{ width: 60, height: 60, alignSelf: 'flex-start' }}
+                        borderRadius={50}
+                    />
+                    <View style={{ marginLeft: 15 }}>
+                        <Text style={style.companyName}>{orderCompany.name}</Text>
+                        <Text style={style.companyUser}>{orderCompany.user}</Text>
+                        <View style={{
+                            borderBottomWidth: 1,
+                            borderBottomColor: '#d9d9d9',
+                            width: "265%",
+                            paddingBottom: 15
+                        }} />
+
+                        <View style={{ marginTop: 10, flexDirection: 'row' }}>
+                            <Text style={style.amount}>1</Text>
+                    <Text style={{ ...style.companyUser, marginLeft: 15, color: '#333' }}>{orderCompany.product}</Text>
+                        </View>
+
+                        <Text style={{ ...style.companyUser, color: '#333', marginTop: 10 }}>{`Total: R$${orderCompany.price},00`}</Text>
+                    </View>
+                </View>
+                    <Text style={{ ...style.companyUser, color: '#333', alignSelf: 'flex-start', marginTop: 5 }}>{orderCompany.date}</Text>
+            </View>
+
+            <View style={style.footer}>
+                <Feather name="check-circle" size={20} color="#469F30" />
+                <Text style={{ ...style.companyUser, color: '#FFF', marginLeft: 10, marginBottom: 2 }}>Concluído</Text>
+
+            </View>
+        </View>
+    );
+}
+
+export default HistoryCard;
---
+++ src/components/HistoryCard/styles.ts
@@ -0,0 +1,48 @@
+import { StyleSheet, Image } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        borderRadius: 10,   
+        backgroundColor: '#e8e8e8',
+        justifyContent: 'space-between',
+        marginBottom: 30
+    },
+    content: {
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 10,
+
+    },
+    companyName: {
+        fontFamily: 'Ubuntu_500Medium',
+        fontSize: 18
+    },
+    companyUser: {
+        fontFamily: 'Ubuntu_400Regular',
+        fontSize: 14,
+        color: '#A0A0A0',
+        marginTop: 2
+    },
+    amount: {
+        fontFamily: 'Ubuntu_700Bold',
+        fontSize: 17,
+        color: '#A0A0A0',
+        width: 15,
+        textAlign: 'center',
+        borderWidth: 1,
+        borderColor: '#A0A0A0',
+        borderRadius: 3
+    },
+    footer: {
+        backgroundColor: '#323232',
+        alignItems: 'center',
+        flexDirection: 'row',
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderBottomRightRadius: 10,
+        borderBottomLeftRadius: 10,
+    }
+})
+
+export default styles;
